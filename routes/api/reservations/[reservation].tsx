f172471eb31e05c90bf0c89afeb46cc7113ed254
meosjin <iammeosjin@gmail.com>
2025-01-18 12:18:37 +0800

    feat: suspend users

--- routes/api/reservations/[reservation].tsx
+++ routes/api/reservations/[reservation].tsx
@@ -1,6 +1,10 @@
 import { Handlers } from '$fresh/server.ts';
+import { authorize } from '../../../library/authorize.ts';
 import ReservationModel from '../../../models/reservation.ts';
+import UserModel from '../../../models/user.ts';
 import omit from 'https://deno.land/x/ramda@v0.27.2/source/omit.js';
+import equals from 'https://deno.land/x/ramda@v0.27.2/source/equals.js';
+import { UserStatus } from '../../../types.ts';
 
 export const handler: Handlers = {
   async PATCH(req, ctx) {
@@ -8,12 +12,30 @@ export const handler: Handlers = {
     const operator = await ReservationModel.get(
       ctx.params.reservation.split(';'),
     );
-    console.log('operator', operator);
-    console.log('body', body);
+
     await ReservationModel.insert({
       ...operator,
       ...omit(['id', 'guid', 'dateTimeCreated'], body),
     });
+
+    if (body.status === 'CANCELLED') {
+      const user = await authorize(req);
+      if (!user) {
+        return new Response('User not authorized', {
+          status: 403,
+          headers: { 'Content-Type': 'application/json' },
+        });
+      }
+      const reservations = await ReservationModel.list().then((reservations) =>
+        reservations.filter((res) =>
+          equals(res.user, [user]) && res.status === 'CANCELLED'
+        )
+      );
+
+      if (reservations.length > 3) {
+        await UserModel.updateUser([user], { status: UserStatus.SUSPENDED });
+      }
+    }
     const headers = new Headers();
     return new Response(null, {
       status: 201, // See Other
